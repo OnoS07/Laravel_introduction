47024628155a8996d6796ae05cc9ee8db00bbfae
大野颯一郎 <soichrooono@gmail.com>
2021-01-08 13:05:36 +0900

    [Fnish]6-1

---
+++ resources/views/person/index.blade.php
@@ -0,0 +1,25 @@
+@extends('layouts.helloapp')
+
+@section('title', 'Person.Index')
+
+@section('menubar')
+    @parent
+    インデックスページ
+@endsection
+
+@section('content')
+    <table>
+        <tr>
+            <th>DATA</th>
+        </tr>
+        @foreach($items as $item)
+            <tr>
+                <td>{{$item->getData()}}</td>
+            </tr>
+        @endforeach
+    </table>
+@endsection
+
+@section('footer')
+    copyright 2020
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -19,19 +19,15 @@ Route::get('/', function () {
 });
 
 Route::get('hello', 'App\Http\Controllers\HelloController@index');
-
 Route::get('hello/show', 'App\Http\Controllers\HelloController@show');
-
 Route::post('hello','App\Http\Controllers\HelloController@post');
-
 Route::get('hello/add','App\Http\Controllers\HelloController@add');
-
 Route::post('hello/add','App\Http\Controllers\HelloController@create');
-
 Route::get('hello/edit','App\Http\Controllers\HelloController@edit');
-
 Route::post('hello/edit','App\Http\Controllers\HelloController@update');
-
 Route::get('hello/del','App\Http\Controllers\HelloController@del');
-
 Route::post('hello/del','App\Http\Controllers\HelloController@remove');
+
+Route::get('person', 'App\Http\Controllers\PersonController@index');
+Route::get('person/find', 'App\Http\Controllers\PersonController@find');
+Route::post('person/find', 'App\Http\Controllers\PersonController@search');
